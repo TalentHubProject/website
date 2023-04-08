fb4a85627fd8799a75fac499544d2acffa0b4de2
Alexis C. Gridel <contact@agdl.dev>
2023-04-08 10:51:58 +0200

    build: integration with partytown for ganalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from 'astro-robots-txt'
+import robotsTxt from 'astro-robots-txt';
+import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://talent-hub.fr/",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), robotsTxt()
-  ],
+  }), sitemap(), robotsTxt(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   vite: {
     ssr: {
       external: ["svgo"]
