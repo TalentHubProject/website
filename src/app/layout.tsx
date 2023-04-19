6061ea1766cb82318bcc51f7672b3bd830c331f6
Alexis C. Gridel <contact@agdl.dev>
2023-04-19 22:43:52 +0200

    feat(layout): grid base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Talent Hub : Réunir les talents, inspirer les projets et la croissance ',
-  description: 'Talent Hub, plateforme innovante de collaboration sur Discord, développement, musique, investissement et plus encore.',
+    title: 'Talent Hub : Réunir les talents, inspirer les projets et la croissance ',
+    description: 'Talent Hub, plateforme innovante de collaboration sur Discord, développement, musique, investissement et plus encore.',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="fr">
-      <body>{children}</body>
-    </html>
-  )
+    return (<html lang="fr">
+    <body>
+    <div className="grid md:grid-cols-8 gap-1 sm:grid-cols-1 p-5">
+        <div className="col-span-1"></div>
+        <div className="col-start-2 col-span-6">{children}</div>
+        <div className="col-span-1"></div>
+    </div>
+    </body>
+    </html>)
 }
