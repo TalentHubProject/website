6fa68fde90cd19fd49c8cc31f57193bab5c657ec
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-04-20 13:33:22 +0200

    feat(navbar): navbar title brand

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useState} from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -9,8 +9,13 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className="inline-flex justify-between w-full">
-                <ul className="inline-flex">
+            <nav className="inline-flex justify-between w-full items-center">
+                <ul className="inline-flex items-center">
+                    <li>
+                        <Link href="/" className="mr-6 font-bold text-2xl">
+                            Talent Hub
+                        </Link>
+                    </li>
                     <li className="mr-6">
                         <Link href="/">Home</Link>
                     </li>
