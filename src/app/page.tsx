308f2fc097b6e8762e2072f229b4e0e29cde8c84
Alexis C. Gridel <contact@agdl.dev>
2023-04-21 00:22:10 +0200

    style(hero): more important parent text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main className="flex justify-center text-center items-center px-auto">
       <section>
-        <h1 className="font-black text-6xl">Unis pour la croissance</h1>
-        <h4 className="text-gray-400 text-3xl font-light">Art, développement, écriture, et bien plus encore...</h4>
+        <h1 className="font-black text-6xl text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-400">Unis pour la croissance</h1>
+        <h4 className="text-gray-400 text-2xl font-regular">Art, développement, écriture, et bien plus encore...</h4>
       </section>
     </main>
   )
