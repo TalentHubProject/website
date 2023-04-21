f77e88f5671f9ccecc0528197bb212ffcf4fc0d2
Alexis C. Gridel <contact@agdl.dev>
2023-04-21 14:16:29 +0200

    feat(sponsors): initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image'
-import {Inter} from 'next/font/google'
-
-
 export default function Home() {
-    return (<main className="flex justify-center text-center items-center px-auto">
+    return (
+        <main className="flex justify-center text-center items-center px-auto flex-col gap-10">
             <section>
                 <h1 className="font-black text-6xl text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-400">Unis
                     pour la croissance</h1>
@@ -13,6 +10,17 @@ export default function Home() {
                     className="bg-white text-black font-medium py-2 px-4 rounded-md mt-5 shadow-lg shadow-gray-600 hover:shadow-none duration-100 ease-in-out delay-100">
                     <a href="https://discord.talent-hub.fr/" target="_blank" rel="noreferrer">Rejoindre le serveur</a>
                 </button>
+            </section>
+            <section>
+                <span className="uppercase text-gray-400 font-regular text-sm">trusted by persons and projects around the world</span>
+                <div className="flex justify-center items-center gap-5 mt-5">
+                    <img src="/images/clients/1.png" alt="client" className="w-20"/>
+                    <img src="/images/clients/2.png" alt="client" className="w-20"/>
+                    <img src="/images/clients/3.png" alt="client" className="w-20"/>
+                    <img src="/images/clients/4.png" alt="client" className="w-20"/>
+                    <img src="/images/clients/5.png" alt="client" className="w-20"/>
+                    <img src="/images/clients/6.png" alt="client" className="w-20"/>
+                </div>
             </section>
         </main>)
 }
