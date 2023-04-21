96867884e114496c79690e8b74eeb2ff43d764fd
Alexis C. Gridel <contact@agdl.dev>
2023-04-21 14:12:53 +0200

    feat(hero): join discord button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 
 
 export default function Home() {
-  return (
-    <main className="flex justify-center text-center items-center px-auto">
-      <section>
-        <h1 className="font-black text-6xl text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-400">Unis pour la croissance</h1>
-        <h4 className="text-gray-400 text-2xl font-regular">Art, développement, écriture, et bien plus encore...</h4>
-      </section>
-    </main>
-  )
+    return (<main className="flex justify-center text-center items-center px-auto">
+            <section>
+                <h1 className="font-black text-6xl text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-400">Unis
+                    pour la croissance</h1>
+                <h4 className="text-gray-400 text-2xl font-regular">Art, développement, écriture, et bien plus
+                    encore...</h4>
+                <button
+                    className="bg-white text-black font-medium py-2 px-4 rounded-md mt-5 shadow-lg shadow-gray-600 hover:shadow-none duration-100 ease-in-out delay-100">
+                    <a href="https://discord.talent-hub.fr/" target="_blank" rel="noreferrer">Rejoindre le serveur</a>
+                </button>
+            </section>
+        </main>)
 }
