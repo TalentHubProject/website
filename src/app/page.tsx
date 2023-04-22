8344e09ca1db0ba78f00393e3465b90d3a2ec82c
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-04-22 09:57:35 +0200

    feat(hero): change content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ export default function Home() {
     return (<section className="flex justify-center text-center items-center px-auto flex-col h-full">
         <div className="inline-flex flex-col justify-around h-full">
             <div>
-                <h1 className="font-black text-6xl text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-400">Unis
-                    pour la croissance</h1>
-                <h4 className="text-gray-400 text-2xl font-regular">Art, développement, écriture, et bien plus
-                    encore...</h4>
+                <h1 className="font-black text-6xl text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-400">Talent Hub : Collaboration et Créativité</h1>
+                <h4 className="text-gray-400 text-2xl font-regular">Réunissant passionnés d'art, de développement, de musique et bien plus encore</h4>
                 <button
                     className="bg-white text-black font-medium py-2 px-4 rounded-md shadow-lg shadow-gray-600 hover:shadow-none duration-100 ease-in-out delay-100 mt-10">
-                    <a href="https://discord.talent-hub.fr/" target="_blank" rel="noreferrer">Rejoindre le serveur</a>
+                    <a href="https://discord.talent-hub.fr/" target="_blank" rel="noreferrer">Rejoindre l'aventure</a>
                 </button>
             </div>
 
