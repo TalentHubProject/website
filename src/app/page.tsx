b0cc05a2d750b28b8578722059ec66bbc40813f5
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-04-27 16:17:09 +0200

    feat(section): add hub section directional arrow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,7 +111,24 @@ export default function Home() {
                 <div className="col-span-1 row-span-1">
                 </div>
                 <div className="col-span-1 row-span-1">
-                    <p>hey2</p>
+                    <div className="flex flex-row">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round" className="lucide lucide-arrow-left">
+                                <line x1="19" x2="5" y1="12" y2="12"></line>
+                                <polyline points="12 19 5 12 12 5"></polyline>
+                            </svg>
+                        </div>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round" className="lucide lucide-arrow-right">
+                                <line x1="5" x2="19" y1="12" y2="12"></line>
+                                <polyline points="12 5 19 12 12 19"></polyline>
+                            </svg>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-span-1 row-span-1">
                     <p>hey3</p>
