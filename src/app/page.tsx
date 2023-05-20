a85e5ef333fc99b223e865d302751a83bb42194b
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-05-20 23:21:18 +0200

    style(hero/heroSection): align to left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/_partials/sections/heroSection";
 
 export default function Home() {
     return (<>
-        <main className="flex justify-center items-center flex-col h-full container-sm mx-auto">
+        <main className="flex justify-center items-center flex-col h-full container mx-auto">
             <HeroSection/>
         </main>
     </>)
