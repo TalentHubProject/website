5500b056eb500ad1576a5cef41cbbe389f90ece8
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-05-20 23:23:02 +0200

    fix(hero/heroSection): container adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/_partials/sections/heroSection";
 
 export default function Home() {
     return (<>
-        <main className="flex justify-center items-center flex-col h-full container mx-auto">
+        <main className="flex justify-center items-center flex-col h-full mt-10">
             <HeroSection/>
         </main>
     </>)
