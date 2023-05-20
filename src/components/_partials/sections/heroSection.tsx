0aa56d38f1bd75d20d92f982dd513f8cb5cc5601
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-05-20 23:13:26 +0200

    style(hero/heroSection): initial commit

---
+++ src/components/_partials/sections/heroSection.tsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+    return (
+        <section className="flex justify-between items-center">
+            <div className="flex items-center justify-between flex-col gap-20 break-words text-left">
+                <h1 className="font-semibold text-6xl">Talent Hub Collaboration et créativité</h1>
+                <h2 className="text-livid text-xl">Réunissant passionés d'art, de développement, de musique et bien plus
+                    encore</h2>
+
+                <div className="inline-flex justify-between items-center gap-5">
+                    <button className="bg-white text-black py-4 px-6 rounded-xl font-semibold">Rejoindre l'aventure
+                    </button>
+                    <button className="bg-black text-white py-4 px-12 rounded-xl font-semibold border border-white">Se
+                        connecter
+                    </button>
+                </div>
+            </div>
+
+            <div>
+                <img src="/discord-platform.png" alt="Overview of the discord server"/>
+            </div>
+        </section>
+    );
+}
