8e910c706bb128c078f8e1d100dbaef77af36609
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-05-21 10:41:02 +0200

    feat(section/sponsor): initial commit

--- src/components/_partials/sections/sponsorSection.tsx
+++ src/components/_partials/sections/sponsorSection.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import Image from "next/image";
+
+interface SponsorSectionProps {
+    image: string;
+    alt: string;
+}
+
 export default function SponsorSection() {
-    return (<section className="h-screen grid grid-cols-1 md:grid-cols-3 bg-white">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid culpa cupiditate, ipsa magni
-                minus molestias officiis quisquam sit. Accusantium dolorem impedit labore rem! Blanditiis debitis
-                ducimus possimus quis rerum?</p>
-        </section>);
+
+    const sponsors: SponsorSectionProps[] = [
+        {
+            image: "/sponsors/df.png",
+            alt: "Logo de Discord France"
+        },
+        {
+            image: "/sponsors/elkir.png",
+            alt: "Logo de l'association Elkir"
+        },
+        {
+            image: "/sponsors/lljs.png",
+            alt: "Logo des Laboratoires JS"
+        },
+        {
+            image: "/sponsors/ltdd.png",
+            alt: "Logo de La Taverne Du Design"
+        }
+    ];
+
+    return (
+        <section className="bg-white text-black w-full shadow-sm h-56">
+            <div className="container mx-auto flex items-center h-full">
+                <span className="font-semibold text-xl">Soutenu par des personnes et projets à travers le monde</span>
+                <div className="flex flex-row gap-5 items-center">
+                    {sponsors.map((sponsor, index) => (
+                        <div key={index} className="flex flex-col justify-center items-center">
+                            <Image src={sponsor.image} width={50} height={50} alt={sponsor.alt} className="grayscale"/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
