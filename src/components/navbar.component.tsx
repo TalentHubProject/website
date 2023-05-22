0ec9b342e81a2b998eccfe743cb5d737a3a49141
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-05-22 09:38:44 +0200

    feat(component/navbar): DOM property to react style

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -37,8 +37,8 @@ export default function Navbar() {
                 <Link href="/sign-in"
                       className="inline-flex justify-between hover:text-white delay-75 duration-100 ease-in-out text-livid text-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                         stroke-linejoin="round" className="lucide lucide-fingerprint">
+                         fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                         strokeLinejoin="round" className="lucide lucide-fingerprint">
                         <path d="M2 12C2 6.5 6.5 2 12 2a10 10 0 0 1 8 4"></path>
                         <path d="M5 19.5C5.5 18 6 15 6 12c0-.7.12-1.37.34-2"></path>
                         <path d="M17.29 21.02c.12-.6.43-2.3.5-3.02"></path>
