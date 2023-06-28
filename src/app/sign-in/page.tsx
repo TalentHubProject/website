fa89e294dd3b980f4967381d3b29109901fc9dcb
Alexis C. Gridel <contact@agdl.dev>
2023-06-28 11:11:17 +0200

    feat: fresh restart

--- src/app/sign-in/page.tsx
+++
@@ -1,8 +0,0 @@
-import {SignIn} from "@clerk/nextjs";
-
-export default function Page() {
-
-    return (<div className="h-full w-full flex justify-center items-center">
-        <SignIn/>
-    </div>)
-}
--- src/app/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-import {SignUp} from "@clerk/nextjs";
-
-export default function Page() {
-    return (<div className="h-full w-full flex justify-center items-center">
-        <SignUp/>
-    </div>)
-}
