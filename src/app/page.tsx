d467370d37931b67361e8fe5d9008616ea733ffc
Alexis C. Gridel <contact@agdl.dev>
2023-06-28 13:40:20 +0200

    style(page): set core layout with a grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import FeatureSection from "@/components/_partials/sections/featureSection";
 import SponsorSection from "@/components/_partials/sections/sponsorSection";
 
 export default function Page() {
-    return (<>
-            <HeroSection/>
-            <SponsorSection/>
-            <FeatureSection/>
-        </>);
+    return (
+        <div className="grid place-items-center p-20">
+            <div className="max-w-xl w-full p-10">
+                <HeroSection/>
+                <SponsorSection/>
+                <FeatureSection/>
+            </div>
+        </div>);
 }
--- src/components/_partials/sections/heroSection.tsx
+++ src/components/_partials/sections/heroSection.tsx
@@ -2,29 +2,10 @@ import Image from "next/image";
 
 export default function HeroSection() {
     return (
-        <section className="h-screen grid grid-cols-1 md:grid-cols-3 container mx-auto ">
-            <div className="flex items-start justify-center flex-col gap-20 break-words text-left col-start-1">
-                <h1 className="font-medium text-6xl">Talent Hub Collaboration et créativité</h1>
-                <h2 className="text-livid text-xl">Réunissant passionés d'art, de développement, de musique et bien plus
-                    encore</h2>
-
-                <div className="inline-flex justify-start items-center gap-5">
-                    <button className="bg-white text-black py-4 px-6 rounded-xl font-semibold">Rejoindre l'aventure
-                    </button>
-                    <button className="bg-black text-white py-4 px-12 rounded-xl font-semibold border border-white">Se
-                        connecter
-                    </button>
-                </div>
-            </div>
-
-            <div className="col-start-2 col-span-3">
-                <Image
-                    src="/discord-platform.png"
-                    alt="Overview of the discord server"
-                    layout="fill"
-                    objectFit="contain"
-                />
-            </div>
+        <section className="flex items-center justify-center">
+            <h1 className="text-9xl bg-clip-text text-transparent font-semibold bg-gradient-to-r from-[#F3F3F3] to-[#E8C7FA]">
+                Talent Hub Collaboration et créativité
+            </h1>
         </section>
     );
 }
