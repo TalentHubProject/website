94c2a8814a757a7b839ae4d701648f05f2e777f4
Alexis C. Gridel <contact@agdl.dev>
2023-06-28 14:03:26 +0200

    feat(section/hero): head and mouth text

--- src/components/_partials/sections/heroSection.tsx
+++ src/components/_partials/sections/heroSection.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 
 export default function HeroSection() {
     return (
-        <section className="flex items-center justify-center">
+        <section className="flex items-center justify-center flex-col">
             <h1 className="text-9xl bg-clip-text text-transparent font-semibold bg-gradient-to-r from-[#F3F3F3] to-[#E8C7FA]">
                 Talent Hub Collaboration et créativité
             </h1>
+            <p className="text-livid pt-5">Réunissant passionnés d'art, de développement, de musique et bien plus encore </p>
         </section>
     );
 }
