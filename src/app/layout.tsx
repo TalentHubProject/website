3343fcd2d5e2aba983c2d3eae8289a825e251e73
Alexis C. Gridel <contact@agdl.dev>
2023-06-28 11:44:27 +0200

    feat(tailwind):

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,27 @@
-import './globals.css'
+import './global.css'
+
 import Navbar from "@/components/navbar.component";
-import {Inter} from "next/font/google";
-import {ClerkProvider} from "@clerk/nextjs";
+
+import {Inter} from 'next/font/google'
 
 export const metadata = {
     title: 'Talent Hub : Réunir les talents, inspirer les projets et la croissance ',
     description: 'Talent Hub, plateforme innovante de collaboration sur Discord, développement, musique, investissement et plus encore.',
-    themeColor: [{media: "(prefers-color-scheme: light)", color: "white"}, {
-        media: "(prefers-color-scheme: dark)", color: "black"
-    },],
 }
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({subsets: ['latin', 'cyrillic-ext']});
 
 export default function RootLayout({
                                        children,
                                    }: {
     children: React.ReactNode
 }) {
-    return (<html lang="fr" className={inter.className}>
-    <body className="min-h-screen bg-primary">
-    <ClerkProvider>
-        <main>
-            <Navbar/>
-            {children}
-        </main>
-    </ClerkProvider>
+    return (<html lang="fr">
+    <body>
+    <main className={inter.className}>
+        <Navbar/>
+        {children}
+    </main>
     </body>
     </html>)
 }
--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,60 +1,15 @@
-"use client";
-
-import React, {useState} from "react";
-import Link from "next/link";
-import Image from "next/image";
-import {SignedIn, SignedOut, UserButton} from "@clerk/nextjs";
-
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
-
-    return (<header className="container mx-auto py-8">
-        <nav className="flex justify-between w-full items-center pb-4">
-            <Link href="/" className="mr-6 font-bold text-xl flex items-center gap-1">
-                <Image src="/logo_white.png" alt="Talent Hub's logo in white version" width={32} height={32}/>
-                <span>Talent Hub</span>
-            </Link>
-            <ul className="inline-flex items-center text-sm text-livid gap-5">
-                <li className="mr-6">
-                    <Link href="/blog" className="hover:text-white delay-75 duration-100 ease-in-out">Blog</Link>
-                </li>
-                <li className="mr-6">
-                    <Link href="/offres" className="hover:text-white delay-75 duration-100 ease-in-out">Offres</Link>
-                </li>
-                <li className="mr-6">
-                    <Link href="/trombinoscope"
-                          className="hover:text-white delay-75 duration-100 ease-in-out">Trombinoscope</Link>
-                </li>
-            </ul>
-
-            <SignedIn>
-                <UserButton/>
-            </SignedIn>
-
-            <SignedOut>
-
-                <Link href="/sign-in"
-                      className="inline-flex justify-between hover:text-white delay-75 duration-100 ease-in-out text-livid text-sm">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                         fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                         strokeLinejoin="round" className="lucide lucide-fingerprint">
-                        <path d="M2 12C2 6.5 6.5 2 12 2a10 10 0 0 1 8 4"></path>
-                        <path d="M5 19.5C5.5 18 6 15 6 12c0-.7.12-1.37.34-2"></path>
-                        <path d="M17.29 21.02c.12-.6.43-2.3.5-3.02"></path>
-                        <path d="M12 10a2 2 0 0 0-2 2c0 1.02-.1 2.51-.26 4"></path>
-                        <path d="M8.65 22c.21-.66.45-1.32.57-2"></path>
-                        <path d="M14 13.12c0 2.38 0 6.38-1 8.88"></path>
-                        <path d="M2 16h.01"></path>
-                        <path d="M21.8 16c.2-2 .131-5.354 0-6"></path>
-                        <path d="M9 6.8a6 6 0 0 1 9 5.2c0 .47 0 1.17-.02 2"></path>
-                    </svg>
-                    <span className="ml-2">
-                                Se connecter
-                            </span>
-                </Link>
-
-            </SignedOut>
+    return (
+        <nav className="w-full flex flex-row justify-between">
+            <div>Talent Hub</div>
+            <div>
+                <ul className="flex flex-row justify-between gap-5">
+                    <li>Blog</li>
+                    <li>Offres</li>
+                    <li>Trombinoscope</li>
+                </ul>
+            </div>
+            <div>Se connecter</div>
         </nav>
-    </header>);
+    );
 }
