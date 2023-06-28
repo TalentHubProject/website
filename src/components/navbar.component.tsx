480d74b67aad8c344ca2bbb20ad44210bcdc6f6b
Alexis C. Gridel <contact@agdl.dev>
2023-06-28 16:15:19 +0200

    style(navbar): anchor to pertinent elements

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,24 +1,30 @@
 import Image from "next/image";
 
 export default function Navbar() {
-    return (
-        <nav className="w-full flex flex-row justify-between px-20 py-10">
-            <div className="text-white font-bold text-xl flex justify-between items-center">
-                <Image src="/logo_white.png" alt="Logo de Talent Hub" width={32} height={32} />
-                <span>
-                    Talent Hub
-                </span>
-            </div>
+    return (<nav className="w-full flex flex-row justify-between px-20 py-10">
+            <a href="/">
+
+                <div className="text-white font-bold text-xl flex justify-between items-center">
+                    <Image src="/logo_white.png" alt="Logo de Talent Hub" width={32} height={32}/>
+                    <span>
+                      Talent Hub
+                  </span>
+                </div>
+            </a>
+
             <div>
                 <ul className="flex flex-row justify-between gap-10 font-semibold text-livid">
-                  <li><a className="transition-colors duration-200 hover:text-indigo-600" href="#">Blog</a></li>
-                  <li><a className="transition-colors duration-200 hover:text-indigo-600" href="#">Offres</a></li>
-                  <li><a className="transition-colors duration-200 hover:text-indigo-600" href="#">Trombinoscope</a></li>
+                    <li><a className="transition-colors duration-200 hover:text-indigo-600" href="#">Blog</a></li>
+                    <li><a className="transition-colors duration-200 hover:text-indigo-600" href="#">Offres</a></li>
+                    <li><a className="transition-colors duration-200 hover:text-indigo-600" href="#">Trombinoscope</a>
+                    </li>
                 </ul>
             </div>
-            <div className="bg-white text-black px-5 py-3 font-semibold rounded-xl">
-                Se connecter
-            </div>
-        </nav>
-    );
+            <a href="#">
+                <div className="bg-white text-black px-5 py-3 font-semibold rounded-xl">
+                    Se connecter
+                </div>
+            </a>
+
+        </nav>);
 }
