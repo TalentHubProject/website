22cc6478ec9f38cb58f99e1b3b1490fae2f988e0
Alexis C. Gridel <contact@agdl.dev>
2023-06-29 10:59:36 +0200

    style(section/hero): center text

--- src/components/_partials/sections/heroSection.tsx
+++ src/components/_partials/sections/heroSection.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 export default function HeroSection() {
     return (<section className="flex items-center justify-center flex-col mt-96">
-        <span className="text-sm px-4 py-2 border-gray-600 border rounded-full text-livid mb-56">Scroll</span>
-            <h1 className="text-9xl bg-clip-text text-transparent font-semibold bg-gradient-to-r from-[#F3F3F3] to-[#E8C7FA]">
-                Talent Hub Collaboration et créativité
-            </h1>
-            <p className="text-livid pt-5">Réunissant passionnés d'art, de développement, de musique et bien plus
-                encore </p>
-        </section>);
+        <span className="text-sm px-4 py-2 border-gray-600 border rounded-full text-livid mb-16">Scroll</span>
+        <h1 className="text-center text-9xl bg-clip-text text-transparent font-semibold bg-gradient-to-r from-[#F3F3F3] to-[#E8C7FA]">
+            Talent Hub Collaboration et créativité
+        </h1>
+        <p className="text-livid pt-5">Réunissant passionnés d'art, de développement, de musique et bien plus
+            encore </p>
+    </section>);
 }
