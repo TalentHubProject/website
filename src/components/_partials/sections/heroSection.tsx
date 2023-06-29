ca9e324765c16f2b6cea1d73a51779d1c3478591
Alexis C. Gridel <contact@agdl.dev>
2023-06-29 11:03:53 +0200

    feat(section/hero): button with hover effect

--- src/components/_partials/sections/heroSection.tsx
+++ src/components/_partials/sections/heroSection.tsx
@@ -4,7 +4,12 @@ export default function HeroSection() {
         <h1 className="text-center text-9xl bg-clip-text text-transparent font-semibold bg-gradient-to-r from-[#F3F3F3] to-[#E8C7FA]">
             Talent Hub Collaboration et créativité
         </h1>
-        <p className="text-livid pt-5">Réunissant passionnés d'art, de développement, de musique et bien plus
+        <p className="text-livid pt-5 text-center">Réunissant passionnés d'art, de développement, de musique et bien plus
             encore </p>
+        <a href="https://discord.talent-hub.fr"
+           target="_blank"
+           className="mt-20 px-5 py-2 bg-white hover:bg-gray-200 rounded-xl font-semibold text-black transition-colors duration-200">
+            Rejoindre l'aventure
+        </a>
     </section>);
 }
