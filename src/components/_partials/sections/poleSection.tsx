ba84bd60b9d8a6bdd2b711074b76de98c75ae202
Alexis C. Gridel <contact@agdl.dev>
2023-07-05 23:18:00 +0200

    feat(section/poles): add arrow buttons

--- src/components/_partials/sections/poleSection.tsx
+++ src/components/_partials/sections/poleSection.tsx
@@ -1,42 +1,118 @@
 import React from "react";
 
 export default function PoleSection() {
-    return (
-        <section className="text-center flex flex-col justify-center items-center">
+    return (<section className="text-center flex flex-col justify-center items-center">
 
-            <div>
-                <span className="text-sm py-2 border-gray-600 border rounded-full text-livid px-6">Rôles</span>
+        <div>
+            <span className="text-sm py-2 border-gray-600 border rounded-full text-livid px-6">Rôles</span>
 
-                <h2 className="text-white text-6xl font-semibold break-normal mt-14 whitespace-nowrap">
-                    {"Les différents pôles que"}<br/> {"nous offrons sur Talent Hub"}
-                </h2>
+            <h2 className="text-white text-6xl font-semibold break-normal mt-14 whitespace-nowrap">
+                {"Les différents pôles que"}<br/> {"nous offrons sur Talent Hub"}
+            </h2>
 
-                <p className="text-livid mt-10">
-                    Talent Hub propose des pôles spécialisés. Cela permet d'améliorer le processus d'acquisition de talents et d'augmenter la qualité de la procédure de recrutement.
-                </p>
+            <p className="text-livid mt-10">
+                Talent Hub propose des pôles spécialisés. Cela permet d'améliorer le processus d'acquisition de
+                talents et d'augmenter la qualité de la procédure de recrutement.
+            </p>
+        </div>
+        <div className="w-screen flex justify-end items-end text-white h-full mt-10 mr-10">
+            <span>{'<='}</span>
+            <span>{'=>'}</span>
+        </div>
+
+        <div className="flex flex-row w-screen overflow-x-hidden">
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-red-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 1
+                </div>
             </div>
-
-            <div className="flex flex-col h-screen overflow-auto scroll-snap-y">
-                <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
-                    <div className="bg-red-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
-                        Carte 1
-                    </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-green-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 2
                 </div>
-                <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
-                    <div className="bg-green-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
-                        Carte 2
-                    </div>
-                </div>
-                <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
-                    <div className="bg-blue-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
-                        Carte 3
-                    </div>
-                </div>
-                {/* Ajoutez plus de cartes si nécessaire */}
             </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-blue-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 3
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-red-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 1
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-green-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 2
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-blue-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 3
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-red-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 1
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-green-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 2
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-blue-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 3
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-red-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 1
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-green-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 2
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-blue-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 3
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-red-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 1
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-green-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 2
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-blue-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 3
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-red-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 1
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-green-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 2
+                </div>
+            </div>
+            <div className="h-screen scroll-snap-start flex justify-center items-center text-white text-5xl">
+                <div className="bg-blue-500 w-1/2 h-1/2 flex justify-center items-center shadow-2xl rounded-lg">
+                    Carte 3
+                </div>
+            </div>
+        </div>
 
 
-
-        </section>
-    );
+    </section>);
 }
