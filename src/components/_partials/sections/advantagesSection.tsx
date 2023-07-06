310eb51a661ae231a0994f6d907eb79707e83528
Alexis C. Gridel <contact@agdl.dev>
2023-07-06 09:45:52 +0200

    feat(section/pole): add arrow icons

--- src/components/_partials/sections/advantagesSection.tsx
+++ src/components/_partials/sections/advantagesSection.tsx
@@ -4,48 +4,46 @@ import { Collapse, Text } from "@nextui-org/react";
 
 const AdvantagesSection = () => {
     return (
-        <section>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur doloribus ducimus expedita fugit
-                itaque maxime obcaecati officia, possimus quae quaerat, quidem quis sapiente similique, totam unde vel
-                veniam veritatis voluptatibus?</p>
+        <section className="text-center flex flex-row justify-center items-center text-livid">
+            <iframe src="https://discord.com/widget?id=1057360697887051806&theme=dark" width="350" height="500" allowTransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
             <Collapse.Group>
                 <Collapse title="Système d'offres">
-                    <Text>
+                    <p>
                        Avec Talent Hub, vous pouvez bénéficier d'un système d'offre unique. Il vous permet
                        de présenter vos services de manière professionnelle et de trouver le talent idéal
                        pour vos projets. Plus qu'un simple tableau d'affichage, notre système est conçu
                        pour faciliter les interactions et la collaboration.
-                    </Text>
+                    </p>
                 </Collapse>
                 <Collapse title="Apprentissage collaboratif">
-                    <Text>
+                    <p>
                         Talent Hub propose une plateforme d'apprentissage collaboratif où les membres peuvent
                         partager leurs connaissances, poser des questions et apprendre les uns des autres. Cette
                         approche communautaire à l'apprentissage facilite un environnement d'éducation continue
                         et d'amélioration des compétences.
-                    </Text>
+                    </p>
                 </Collapse>
                 <Collapse title="Concours et Challenges">
-                    <Text>
+                    <p>
                         Pour stimuler l'innovation et la créativité, Talent Hub organise régulièrement des concours
                         et des défis de codage. Ces événements vous offrent une occasion unique de montrer vos compétences,
                         d'apprendre de nouvelles techniques et de recevoir des commentaires constructifs de la part de
                         notre communauté d'experts.
-                    </Text>
+                    </p>
                 </Collapse>
                 <Collapse title="Réseau professionnel">
-                    <Text>
+                    <p>
                         En rejoignant Talent Hub, vous accédez à un vaste réseau de développeurs et de professionnels de l'industrie.
                         C'est une excellente occasion de se connecter avec des pairs, de trouver des mentors, de collaborer sur des projets,
                         et même de découvrir de nouvelles opportunités de carrière.
-                    </Text>
+                    </p>
                 </Collapse>
                 <Collapse title="Ressources de développement personnel">
-                    <Text>
+                    <p>
                         Talent Hub ne se limite pas à la programmation. Nous mettons à disposition une variété de ressources pour le développement
                         personnel, y compris des ateliers sur les soft skills, des séances de coaching de carrière, des discussions sur le bien-être
                         mental et plus encore. Nous croyons fermement que le succès en tant que développeur va au-delà des compétences techniques.
-                    </Text>
+                    </p>
                 </Collapse>
             </Collapse.Group>
         </section>
