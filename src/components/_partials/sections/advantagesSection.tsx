cc8e73fc8b176ed0952d6ff0584760675e1e02c0
Alexis C. Gridel <contact@agdl.dev>
2023-07-06 15:43:24 +0200

    fix(section/advantage): dom property

--- src/components/_partials/sections/advantagesSection.tsx
+++ src/components/_partials/sections/advantagesSection.tsx
@@ -4,7 +4,7 @@ const AdvantagesSection = () => {
     return (<section
             className="flex flex-row justify-center items-center text-center space-x-10 text-livid px-4 sm:px-6 lg:px-8 py-10 w-full mx-auto">
             <iframe src="https://discord.com/widget?id=1057360697887051806&theme=dark" width="350" height="500"
-                    allowTransparency="true" frameBorder="0"
+                    allowTransparency={true} frameBorder="0"
                     sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </section>
 
