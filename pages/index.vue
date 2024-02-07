dcc0aadd87eb913f980a4c0e999854a3ea73c7ea
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-07 17:51:29 +0100

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,287 @@
+<template class="bg-black">
+    <div>
+        <div class="min-h-screen flex justify-center align-middle">
+            <section class="bg-white">
+                <div
+                    class="mt-16 lg:mt-40 2xl:mt-64 pt-32 px-2 mx-auto max-w-screen-lg text-center md:bg-white lg:pt-16 lg:px-12">
+
+                    <h1
+                        class="mb-8  text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:mb-16  lg:text-6xl ">
+                        Talent Hub Collaboration et créativité</h1>
+                    <p class=" text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Réunissant
+                        passionnés d'art, de développement, de musique et bien plus encore</p>
+                    <div
+                        class="flex flex-col mb-8   lg:mb-10 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+
+                    </div>
+                    <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+                        <span class="font-semibold mb-8 text-gray-400 uppercase">
+                            <JoinButton text="Rejoindre l’aventure" />
+                        </span>
+                        <div
+                            class="flex flex-wrap justify-around sm:justify-center items-center mt-32 text-gray-500 sm:justify-around">
+                            <a href="#" class=" lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
+                                <img class="" src="~/assets/images/DISCORD FRANCE.png" />
+                            </a>
+                            <a>
+                                <img class="" src="~/assets/images/lljs.png" />
+                            </a>
+                            <a>
+                                <img class="" src="~/assets/images/elkir.png" />
+                            </a>
+                            <a>
+                                <img class="" src="~/assets/images/ltdd.png" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+        <section class="mt-8 pb-8 md:mt-32 ">
+            <div class="border-solid md:border-2 border-blue-500 m-3 rounded-xl">
+                <div class="mt-2 md:16 pt-32 px-4 mx-auto max-w-screen-xl text-center lg:pt-16 ">
+                    <div class="flex items-center justify-center ">
+                        <div class="bg-slate-200 px-4 py-1 rounded text-slate-700">
+                            <img class="float-left pt-1 pr-2" src="~/assets/images/file.png" />
+                            A propos
+                        </div>
+                    </div>
+                    <h1 class="text-2xl md:text-3xl pt-12 font-bold">Qu'est-ce que Talent Hub ?</h1>
+                    <p class=" text-lg pt-8 font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-64 dark:text-gray-400">
+                        Découvrez
+                        Talent Hub, la plateforme collaborative ultime qui rassemble les esprits créatifs et innovants.</p>
+                    <div class="px-0 md:px-12 mt-20 md:pb-16 grid grid-cols-1 gap-16 md:grid-cols-3 md:gap-5 text-left">
+                        <div>
+                            <img class="" src="~/assets/images/Ellipse1.png" />
+                            <h3 class="pt-4 text-2xl font-semibold">Contenu</h3>
+                            <p class="pt-4 pb-4 font-normal text-xl text-gray-400">Suivez des créations ou un modèle créé
+                                par
+                                des artistes vérifiés et accélérez la croissance de votre niveau et de votre créativité.</p>
+                        </div>
+                        <div>
+                            <img class="" src="~/assets/images/Ellipse2.png" />
+                            <h3 class="pt-4 text-2xl font-semibold">Communauté</h3>
+                            <p class="pt-4 pb-4 font-normal text-xl text-gray-400">Rejoignez Talent Hub et trouvez les
+                                connaissances, le soutien et la communauté qui vous aideront à faire évoluer votre
+                                entreprise
+                                vers le niveau suivant.</p>
+                        </div>
+                        <div>
+                            <img class="" src="~/assets/images/Ellipse3.png" />
+                            <h3 class="pt-4 text-2xl font-semibold">Besoin d’aide</h3>
+                            <p class="pt-4 pb-4 font-normal text-xl text-gray-400">Recevez des conseils de plusieurs
+                                artistes en
+                                groupe ou en tête-à-tête et surmontez les obstacles commerciaux et les blocages créatifs.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <section class="polygon w-full">
+            <div class="mt-0  pt-48 md:pt-16 pb-32 px-4 mx-auto max-w-screen-xl text-center lg:pt-80 ">
+                <div class="flex items-center justify-center ">
+                    <div class="Polos px-4 py-1 rounded">
+                        <img class="float-left pt-1 pr-2" src="~/assets/images/poles.png" />
+                        <p class=" float-left">Pôles</p>
+                    </div>
+                </div>
+                <h1 class="text-2xl px-0 pt-6 font-bold mx-0 text-white md:px-64">Venez partager, échanger, évoluer dans vos
+                    domaines respectifs.</h1>
+                <div
+                    class="px-0 sm:5px lg:px-20 md:px-40  pt-16  grid grid-cols-1 gap-24 md:grid-cols-3 md:gap-5 text-left">
+                    <div
+                        class="mx-8 md:mx-0 border border-white rounded-md  text-white hover:text-black text-white hover:text-black hover:bg-white">
+                        <img class="ml-auto" src="~/assets/images/reading-side.png" />
+                        <h3 class="pl-4 pt-4 text-sm font-bold">Graphisme</h3>
+                        <p class="ml-4 w-60 pb-4 font-normal text-sm ">Tu es graphiste ou fan de
+                            ce domaine ? Tu
+                            es au bon endroit !</p>
+                    </div>
+                    <div class="mx-8 md:mx-0 border border-white rounded-md text-white hover:text-black hover:bg-white">
+                        <img class="ml-auto" src="~/assets/images/dancing.png" />
+                        <h3 class="pl-4 text-sm font-bold">Musique</h3>
+                        <p class="ml-4 w-60 pb-4 font-normal text-sm ">Tu es musicien ou fan de ce domaine? Tu es
+                            au bon endroit !</p>
+                    </div>
+                    <div class="mx-8 md:mx-0 border border-white rounded-md text-white hover:text-black hover:bg-white">
+                        <img class="ml-auto" src="~/assets/images/clumsy 1.png" />
+                        <h3 class="pl-4 text-sm font-bold ">Investissement</h3>
+                        <p class="ml-4 w-60 pb-4 font-normal text-sm ">Tu es investisseur.se ou fan de ce domaine?
+                            Tu es au bon endroit !</p>
+                    </div>
+                </div>
+                <div
+                    class="px-0 lg:px-20 md:px-40 pt-24 md:pt-12 pb-44 grid grid-cols-1 gap-24 md:grid-cols-3 md:gap-5 text-left">
+                    <div class="mx-8 md:mx-0 border border-white rounded-md text-white hover:text-black hover:bg-white">
+                        <img class="ml-auto" src="~/assets/images/Combined-Shape.png" />
+                        <h3 class="pl-4 pt-5 text-sm font-bold ">Développement</h3>
+                        <p class="ml-4 w-60 pb-4 font-normal text-sm ">Tu es développeur.se ou fan de ce domaine?
+                            Tu es au bon endroit !</p>
+                    </div>
+                    <div class="mx-8 md:mx-0 border border-white rounded-md text-white hover:text-black hover:bg-white">
+                        <img class="ml-auto" src="~/assets/images/sitting-reading 1.png" />
+                        <h3 class="pl-4 text-sm font-bold ">Écriture</h3>
+                        <p class="ml-4 w-60 pb-4 font-normal text-sm ">Tu es écrivain.ne ou fan de ce domaine? Tu
+                            es au bon endroit !</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="w-full">
+            <div class="mt-32 sm:mt-16 pb-32 px-4 mx-auto max-w-screen-xl text-center lg:pt-20 ">
+                <div class="flex items-center justify-center ">
+                    <div class="bg-slate-100 px-4 py-1 rounded">
+                        <img class="float-left pt-1 pr-2" src="~/assets/images/faq.png" />
+                        <p class="text-slate-700 float-left">FAQ</p>
+                    </div>
+                </div>
+                <h1 class="text-2xl sm:text-3xl pt-6 font-bold mx-12 text-black xl:px-64">Question fréquemment demandé</h1>
+                <div class="px-0 lg:px-16 pt-24 grid grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-24 text-left">
+                    <div class="">
+                        <div>
+                            <h3 class="font-bold text-2xl">What is Burst?</h3>
+                            <p class="font-normal font-xl pt-3 text-gray-400">
+                                Burst is a free stock photo platform that is powered by Shopify. Our image library includes
+                                thousands of high-resolution, royalty-free images that were shot by our global community of
+                                photographers. We built Burst to provide designers, developers, bloggers and entrepreneurs
+                                with access to beautiful free stock photography. You can use our pictures for just about
+                                anything — your website, blog or online store, school projects, Instagram ads, facebook
+                                posts, desktop backgrounds, client work and more. All our photos are free for commercial use
+                                with no attribution required; you can read more about this on our license page.
+                            </p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold text-2xl  pt-12 lg:pt-8">How do I download free photos?</h3>
+                            <p class="font-normal font-xl pt-3 text-gray-400">
+                                When you hover over or click on an image, you will see "High Resolution" and "Low
+                                Resolution" download options. "Low Res" images are great for small web graphics, where file
+                                sizes need to be very compressed. "High Res" images are better for presentations, websites,
+                                online stores, and anywhere else where quality matters most.
+                            </p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold text-2xl pt-12 lg:pt-8">Can I edit, crop or resize Burst photos?</h3>
+                            <p class="font-normal font-xl pt-3 text-gray-400">
+                                Yes, you’re welcome to crop, resize, add text and filters, or otherwise modify our stock
+                                photos as you see fit. If you don’t have Photoshop, there are lots of free or low cost
+                                online graphics editors that you can use instead. Feel free to add your logo or brand to our
+                                images to make them your own.
+                            </p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold text-2xl pt-12 lg:pt-8">Can I use these pictures for another online store
+                                platform?</h3>
+                            <p class="font-normal font-xl pt-3 text-gray-400">
+                                Yes! We believe in making commerce better for everyone, so these stock photos are free to
+                                use on any online store platform or ecommerce marketplace. Although they were specifically
+                                created and curated with Shopify stores in mind, it’s really up to you how you want to use
+                                them.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div>
+                            <h3 class="font-bold text-2xl">Do I need to credit the site or photographer?</h3>
+                            <p class="font-normal font-xl pt-3 text-gray-400">
+                                Our photographers appreciate getting credit for their work because it helps share their
+                                photography with more people, so we’d encourage you to do so whenever possible. You can give
+                                credit by linking back to the original photo page or the photographer's Burst profile (e.g.
+                                “Photo by Matthew Henry from Burst”). We realize it's not always possible to provide
+                                attribution, so it is not mandatory under our image license.
+                            </p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold text-2xl pt-12 lg:pt-8">Are these photos free for commercial use?</h3>
+                            <p class="font-normal font-xl pt-3 text-gray-400">
+                                Yes! In fact, we’d highly encourage it, as many of our collections were created with
+                                entrepreneurs in mind. You can use these free photos on your online store, product
+                                packaging, or anywhere else you’d like. You can even use these pics to create t-shirts,
+                                mugs, or other products.
+                            </p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold text-2xl pt-12 lg:pt-8">Can I use these photographs for offline marketing
+                                campaigns?</h3>
+                            <p class="font-normal font-xl pt-3 text-gray-400">
+                                When you hover over or click on an image, you will see "High Resolution" and "Low
+                                Resolution" download options. "Low Res" images are great for small web graphics, where file
+                                sizes need to be very compressed. "High Res" images are better for presentations, websites,
+                                online stores, and anywhere else where quality matters most.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="text-center rounded-lg mx-4 sm:mx-28 mb-16 sm:mt-32"
+                style="background: linear-gradient(90deg, rgba(223,143,144,1) 0%, rgba(187,46,89,1) 100%);">
+                <div class="py-28">
+                    <h1 class="text-2xl lg:text-3xl font-bold mx-0 lg:mx-12 text-white xl:px-72">Les meilleurs créateurs
+                        sont
+                        chez Talent Hub ! </h1>
+                    <button @mouseover="isMouseOver = true" @mouseout="isMouseOver = false"
+                        class="mt-12 bg-white text-[#E05880] text-sm font-medium hover:text-white py-2 px-4  hover:border-transparent rounded"
+                        :class="isMouseOver ? 'red-fill' : ''">
+                        <p class="float-left">Rejoindre l’aventure</p>
+                        <svg width="24" height="21" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M12.97 15.4698C12.8963 15.5385 12.8372 15.6213 12.7962 15.7133C12.7552 15.8053 12.7332 15.9046 12.7314 16.0053C12.7296 16.106 12.7482 16.2061 12.7859 16.2994C12.8236 16.3928 12.8797 16.4777 12.951 16.5489C13.0222 16.6201 13.107 16.6762 13.2004 16.714C13.2938 16.7517 13.3938 16.7702 13.4945 16.7684C13.5952 16.7667 13.6945 16.7446 13.7865 16.7036C13.8785 16.6626 13.9613 16.6035 14.03 16.5298L18.03 12.5298C18.0998 12.4603 18.1552 12.3778 18.1929 12.2868C18.2307 12.1959 18.2502 12.0983 18.2502 11.9998C18.2502 11.9014 18.2307 11.8038 18.1929 11.7129C18.1552 11.6219 18.0998 11.5393 18.03 11.4698L14.03 7.46985C13.9613 7.39616 13.8785 7.33706 13.7865 7.29606C13.6945 7.25507 13.5952 7.23303 13.4945 7.23126C13.3938 7.22948 13.2938 7.248 13.2004 7.28572C13.107 7.32344 13.0222 7.37959 12.951 7.45081C12.8797 7.52203 12.8236 7.60686 12.7859 7.70025C12.7482 7.79364 12.7296 7.89366 12.7314 7.99437C12.7332 8.09507 12.7552 8.19439 12.7962 8.28638C12.8372 8.37838 12.8963 8.46118 12.97 8.52985L15.69 11.2498L6 11.2498C5.80109 11.2498 5.61032 11.3289 5.46967 11.4695C5.32901 11.6102 5.25 11.8009 5.25 11.9998C5.25 12.1988 5.32901 12.3895 5.46967 12.5302C5.61032 12.6708 5.80108 12.7498 6 12.7498L15.69 12.7498L12.97 15.4698Z"
+                                :fill="isMouseOver ? 'white' : '#E05880'" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            isMouseOver: false
+        };
+    }
+};
+</script>
+  
+<style scoped>
+.red-fill {
+    background: linear-gradient(to right, #DF8F90, #E05880);
+    color: #fff;
+}
+
+.container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
+.polygon {
+    clip-path: polygon(0 28%, 100% 7%, 100% 79%, 0 100%);
+    background: linear-gradient(90deg, rgba(193, 65, 104, 1) 13%, rgba(223, 143, 144, 1) 100%);
+}
+
+.Polos {
+    background-color: rgb(241 245 249 / 39%);
+    border: 1px solid #F1F5F94D;
+}
+
+@media screen and (max-width: 426px) {
+
+    /* Apply your polygon shape to the element */
+    .polygon {
+        position: relative;
+        clip-path: polygon(0 7%, 100% 0, 100% 92%, 0 100%);
+        background: linear-gradient(to top, rgba(193, 65, 104, 1) 13%, rgba(223, 143, 144, 1) 100%);
+
+    }
+}
+</style>
