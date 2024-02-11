751727cc9bfcfae35c6b3518687595551cbd69a5
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-11 09:38:24 +0100

    fix(tailwind): set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import {defineConfig} from 'astro/config';
-import tailwind from '@astro/tailwind';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
